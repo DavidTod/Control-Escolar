2346c8fb1c0d9c0b9a888e6ff057bb79c804a7a0
DannyCahuantzi <danny.cahuantzi@gmail.com>
2013-09-19 19:27:13 -0500

    estructura admin
    es un modelo solamente, estaba intentando recordar estructuras

--- Funciones/admin.c
+++ Funciones/admin.c
@@ -4,8 +4,46 @@
 int main(int argc, char *argv[])
 {
     
-  printf("HOLA MUNDO");
-  printf("Danny");
-  system("PAUSE");	
+  typedef struct Admin{
+	int numero, edad, i, n;
+	char nombre[31];
+	float estatura, peso;
+ }; 
+
+int main(){
+    
+
+    char desea;
+
+Admin RegAdmin;
+
+do {
+printf("Numero: ");
+scanf ("%d", &RegAdmin.nombre);
+printf("Dame nombre: ");
+fflush (stdin);
+gets (RegPersona.nombre);
+printf("Dame edad: ");
+scanf ("%d", &RegPersona.edad);
+printf("Dame estatura: ");
+scanf ("%f", &RegPersona.estatura);
+printf("Dame peso: ");
+scanf ("%f", &RegPersona.peso);
+system ("cls");
+
+
+printf ("Clave: %d \n", RegPersona.numero);
+printf ("Nombre: %s \n", RegPersona.nombre);
+printf ("Edad: %d \n", RegPersona.edad);
+printf ("Estatura: %.2f \n", RegPersona.estatura);
+printf ("Peso: %.2f \n", RegPersona.peso);	
+
+
+printf ("Desea procesar otro empleado? S---N \n");
+fflush (stdin);
+desea=getchar();
+
+}while(desea == 's');
+	
   return 0;
 }
